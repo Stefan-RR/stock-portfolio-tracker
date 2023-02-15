e6fba87be177c11e03d44bfdbfdfb58b593d37d8
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-15 13:51:04 -0700

    Create Stock.js

---
+++ src/componenets/Stock.js
@@ -0,0 +1,10 @@
+function Stock({ stockName, quantity }) {
+    return (
+        <div>
+            <input type='text'></input>
+            <input type='number'></input>
+        </div>
+    )
+}
+
+export default Stock
