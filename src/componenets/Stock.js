64fb1a6b36faf8a2e95559980ef781976306f12c
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-20 13:46:04 -0700

    Update Stock.js

--- src/componenets/Stock.js
+++ src/componenets/Stock.js
@@ -1,8 +1,8 @@
 function Stock({ stockName, quantity }) {
     return (
         <div>
-            <input type='text'></input>
-            <input type='number'></input>
+            <input type='text' onChange={stockName}></input>
+            <input type='number' onChange={quantity}></input>
         </div>
     )
 }
