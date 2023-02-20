49e275dbf402c25e0201d9442009df035c3f4df4
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-20 13:46:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Display from './componenets/Display';
 import Stock from './componenets/Stock'
 import Holdings from './componenets/Holdings'
 
 function App() {
+
+  const [symbol, setSymbol] = useState();
+
+  useEffect(() =>{
+    fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=9FYPSKB2VPFSVLU5`)
+    .then(res =>res.json())
+      .then(data => {
+      })
+  }, [symbol])
+
   return (
     <div className="App">
       <span className='top'>
         <Display />
       </span>
       <span className='bottom'>
-        <Stock />
+        <Stock
+          stockName={setSymbol}
+        />
         <Holdings />
       </span>
     </div>
