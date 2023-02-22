d586343ef0e5aed1ea4098c7118e4f843b521473
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-02-21 17:33:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import Holdings from './componenets/Holdings'
 function App() {
 
   const [symbol, setSymbol] = useState();
+  const [shares, setShares]  = useState();
+  const [newPrice, setNewPrice] = useState();
 
   useEffect(() =>{
+    if (setSymbol !=null) {
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=9FYPSKB2VPFSVLU5`)
     .then(res =>res.json())
       .then(data => {
+        console.log(data)
       })
+    }
   }, [symbol])
 
   return (
@@ -22,9 +27,12 @@ function App() {
       </span>
       <span className='bottom'>
         <Stock
-          stockName={setSymbol}
+          stockName={e => setSymbol(e.target.value)}
+          quantity={e => setShares(e.target.value)}
         />
-        <Holdings />
+        <Holdings
+
+         />
       </span>
     </div>
   );
