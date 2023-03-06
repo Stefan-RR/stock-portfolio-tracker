75cd6bec1646fd78f3717303908d5952af9eb53b
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-03-06 15:18:26 -0700

    Update Stock.js

--- src/componenets/Stock.js
+++ src/componenets/Stock.js
@@ -1,19 +1,21 @@
 import { useState } from "react"
 
-function Stock({ stockName, quantity, addStock }) {
-    const [text, setText] = useState("")
+function Stock({ addStock }) {
+    const [stockName, setStockName] = useState("")
+    const [quantity, setQuantity] = useState("")
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        addStock(text)
+        addStock(stockName, quantity)
     }
 
 
     return (
         <form onSubmit={handleSubmit} className="form">
             <label>Stock Name</label>
-            <input type='text' value={text} onChange={(e) => setText(e.target.value)}></input>
+            <input type='text' value={stockName} onChange={(e) => setStockName(e.target.value)}></input>
             <label>Number of Shares</label>
-            <input type='number' onChange={quantity}></input>
+            <input type='number' value={quantity} onChange={(e) => setQuantity(e.target.value)}></input>
             <button type='submit'>Add</button>
         </form>
     )
