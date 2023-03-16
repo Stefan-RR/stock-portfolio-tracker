b44aee0a74340e3d0c50761f680ba75e54526afc
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-03-16 13:24:37 -0600

    Update Stock.js

--- src/componenets/Stock.js
+++ src/componenets/Stock.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function Stock({ addStock }) {
     const [stockName, setStockName] = useState("")
@@ -8,12 +8,22 @@ function Stock({ addStock }) {
         e.preventDefault()
         addStock(stockName, quantity)
     }
-
+    const handleName = (e) => {
+        setStockName(e.target.value)
+        fetch(`https://finnhub.io/api/v1/quote?symbol=${stockName}&token=cfpsonhr01qmi6j4b280cfpsonhr01qmi6j4b28g`)
+                .then(res =>res.json())
+                  .then(data => {
+                    console.log(data)
+                    console.log(stockName)
+                  }) 
+                
+                
+    }
 
     return (
         <form onSubmit={handleSubmit} className="form">
             <label>Stock Name</label>
-            <input type='text' value={stockName} onChange={(e) => setStockName(e.target.value)}></input>
+            <input type='text' value={stockName} onChange={handleName}></input>
             <label>Number of Shares</label>
             <input type='number' value={quantity} onChange={(e) => setQuantity(e.target.value)}></input>
             <button type='submit'>Add</button>
