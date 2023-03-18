6b05be3af2db3365646b6135bbd3b86ae01dab61
Stefan Ruchlewicz <104389039+Stefan-RR@users.noreply.github.com>
2023-03-17 19:45:24 -0600

    Update Stock.js

--- src/componenets/Stock.js
+++ src/componenets/Stock.js
@@ -9,21 +9,20 @@ function Stock({ addStock }) {
         addStock(stockName, quantity)
     }
     const handleName = (e) => {
-        setStockName(e.target.value)
+       
         fetch(`https://finnhub.io/api/v1/quote?symbol=${stockName}&token=cfpsonhr01qmi6j4b280cfpsonhr01qmi6j4b28g`)
                 .then(res =>res.json())
                   .then(data => {
                     console.log(data.c)
                     console.log(stockName)
                   }) 
-                
-                
     }
+
 
     return (
         <form onSubmit={handleSubmit} className="form">
             <label>Stock Name</label>
-            <input type='text' value={stockName} onChange={handleName}></input>
+            <input type='text' value={stockName} onChange={(e) => setStockName(e.target.value)} onBlur={handleName}></input>
             <label>Number of Shares</label>
             <input type='number' value={quantity} onChange={(e) => setQuantity(e.target.value)}></input>
             <button type='submit'>Add</button>
